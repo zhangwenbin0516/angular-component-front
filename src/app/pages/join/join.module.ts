6246e3508537421ea551774f0ec0cc6814f252f2
ZhangWenbin <942623159@qq.com>
2019-03-04 23:47:40 +0800

    'angular'

---
+++ src/app/pages/join/join.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JoinComponent } from './join.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [JoinComponent]
+})
+export class JoinModule { }
